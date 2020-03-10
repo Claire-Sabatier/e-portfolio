b67cb49ccd6c0af9e73fddfd9394d11759d8898e
Claire-Sabatier <claire.sabatier@etu.umontpellier.fr>
2020-03-10 15:48:59 +0100

    Quelques ajouts

--- index.js
+++ index.js
@@ -9,3 +9,14 @@ $(document).ready(function(){
 });
 
 AOS.init();
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.modal').modal();
+  });
